6b789fc7987026878c03cf02fa40553a1065a5e0
David Zhang <dzhangd95@gmail.com>
2017-11-30 22:47:38 -0800

    Read file from given input and add error checking if no valid board can be found

--- Main.hs
+++ Main.hs
@@ -1,20 +1,25 @@
 module Main where
 
 import System.IO
+import System.Environment
 import Data.List
+import Solver
 
--- TODO: compile into CLI
--- TODO: read from file
--- TODO: print out grid
 -- TODO: stretch: print out merchants
 
 main :: IO()
 main = do
-          content <- readFile "input.txt"
-          let list = lines content
-          let merchantVisits = [map read $ words s | s <- list] :: [[Int]]
-          -- let board = solve merchantVisits and print that instead
-          printBoard merchantVisits
+    args <- getArgs
+    case args of
+        [file] -> do
+            content <- readFile file
+            let list = lines content
+            let merchantVisits = [map read $ words s | s <- list] :: [[Int]]
+            let maybeBoard = solve merchantVisits
+            case maybeBoard of
+                Just board -> printBoard board
+                Nothing -> putStrLn "No valid Zed map for these merchant visits"
+        _ -> putStrLn "Wrong number of arguments"
 
 
 printBoardLine :: Show a => [a] -> IO ()
--- Solver.hs
+++ Solver.hs
@@ -2,60 +2,42 @@ module Solver where
 
 import Data.List
 
-solve :: [[Int]] -> [[Int]]
+-- solve takes in a list of lists of Ints representing the list of merchant visits,
+-- with each list depicting one edge of the map, starting from the North side and progressing 
+-- clockwise. Returns a valid Kingdom of Zed map as a matrix.
+solve :: [[Int]] -> Maybe [[Int]]
 solve merchantVisits =
     findValidMap possibleMaps merchantVisits
-    where
-        n = length (head merchantVisits)
-        possibleMaps = generatePossibleMaps n
-        findValidMap (map:maps) visits
-            | isValidMap map visits = map
-            | otherwise = findValidMap maps visits
+  where
+    n = length (head merchantVisits)
+    possibleMaps = generatePossibleMaps n
 
+
+-- findValidMap takes in a list of Zed maps and finds the first one that is valid
+-- according to the Zed rules. Will return a Maybe of a valid map.
+findValidMap :: (Num a, Ord a) => [[[a]]] -> [[a]] -> Maybe [[a]]
+findValidMap [] _ = Nothing
+findValidMap (map:maps) visits
+    | isValidMap map visits = Just map
+    | otherwise = findValidMap maps visits
+
+
+-- generatePossibleMaps takes in a Int representing the lengnth of a side of the Zed map.
+-- It returns a list of all possible Zed maps with that side length.
 generatePossibleMaps :: Int -> [[[Int]]]
-generatePossibleMaps n = concatMap permutations (choose (permutations [1..n]) []) where
+generatePossibleMaps n = concatMap permutations (choose (permutations [1..n]) [])
+  where
     choose [] result = if n == length result then [result] else []
     choose (x:xs) result
         | n == length result = [result]
         | otherwise = choose xs (x:result) ++ choose xs result
 
 
-{- isValid [[4,1,3,2],[2,3,4,1],[3,2,1,4],[1,4,2,3]] [[1,3,2,2],[3,2,1,2],[2,2,1,3],[2,2,3,1]]
-                -}
-
--- TODO:
--- takes a board [[a]] and the merchant visits [[a]] and return
---  a bool for whether the map is valid or not
+-- isValidMap takes a board in the form of a matrix and the merchant visits and returns
+-- whether the map is valid or not according to rules of Zed.
 isValidMap :: (Num a, Ord a) => [[a]] -> [[a]] -> Bool
-isValidMap board merchantVisits = (validBoard board) && (validMerchants board merchantVisits)
-
-
-{- isValid   [ [1,2,3,4]
-                [2,3,4,1]
-                [3,4,1,2]
-                [4,1,2,3] ]
-                -}
-
--- verifies that the map is all unique rows and columns
-validBoard :: Ord a => [[a]] -> Bool
-validBoard board =
-  (foldr (\x y -> allUnique x && y) True rows ) &&
-  (foldr (\x y -> allUnique x && y) True columns)
-  where
-    rows = board
-    columns = transpose board
-
--- helper for verifying all elements in a list are unique
-allUnique :: Ord t => [t] -> Bool
-allUnique [] = True
-allUnique (h:t)
-  | elem h t  = False
-  | otherwise = allUnique t
-
--- verifies that the merchant visits are satisfied by the board
-validMerchants :: (Num a, Ord a) => [[a]] -> [[a]] -> Bool
-validMerchants board visits =
-  and ( (zipWith (\x y -> numVisits y 0 == x) north n_dir) ++
+isValidMap board merchantVisits =
+    and ( (zipWith (\x y -> numVisits y 0 == x) north n_dir) ++
         (zipWith (\x y -> numVisits y 0 == x) west  w_dir) ++
         (zipWith (\x y -> numVisits y 0 == x) south s_dir) ++
         (zipWith (\x y -> numVisits y 0 == x) east  e_dir)  )
@@ -64,14 +46,15 @@ validMerchants board visits =
     n_dir = transpose board
     e_dir = map reverse w_dir
     s_dir = map reverse n_dir
-    north = visits !! 0
-    east = visits !! 1
-    south = reverse (visits !! 2)
-    west = reverse (visits !! 3)
+    north = merchantVisits !! 0
+    east = merchantVisits !! 1
+    south = reverse (merchantVisits !! 2)
+    west = reverse (merchantVisits !! 3)
 
--- helper that returns how many visits a merchant would make
+
+-- numVisits returns how many visits a merchant would make
 numVisits :: (Num a, Ord a) => [a] -> a  -> a
 numVisits [] _ = 0
 numVisits (h:t) n
-  | n < h  = 1 + numVisits t h
-  | otherwise = numVisits t n
+    | n < h  = 1 + numVisits t h
+    | otherwise = numVisits t n
