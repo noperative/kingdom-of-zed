56dc567a3056e65ca7f754a0fbed305d2d7c28e2
noperative <noperative.rx@gmail.com>
2017-11-30 20:58:59 -0800

    printing the board works, only need solve function

--- Main.hs
+++ Main.hs
@@ -1,9 +1,24 @@
 module Main where
 
+import System.IO
+import Data.List
+
 -- TODO: compile into CLI
 -- TODO: read from file
 -- TODO: print out grid
 -- TODO: stretch: print out merchants
 
 main :: IO()
-main = putStrLn "hello, world"
+main = do
+          content <- readFile "input.txt"
+          let list = lines content
+          let merchantVisits = [map read $ words s | s <- list] :: [[Int]]
+          -- let board = solve merchantVisits and print that instead
+          printBoard merchantVisits
+
+
+printBoardLine :: Show a => [a] -> IO ()
+printBoardLine line = putStrLn (unwords (map show line))
+
+printBoard :: (Foldable t) => t [Int] -> IO ()
+printBoard board = mapM_ printBoardLine board
