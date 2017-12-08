414c5eb8d68f568dd371262c87e7981ddf1bdc0a
David Zhang <dzhangd95@gmail.com>
2017-12-07 19:28:10 -0800

    Re-added constraints for non-unique columns

--- Solver.hs
+++ Solver.hs
@@ -17,9 +17,9 @@ solve merchantVisits =
 -- according to the Zed rules. Will return a Maybe of a valid map.
 findValidMap :: (Num a, Ord a) => [[[a]]] -> [[a]] -> Maybe [[a]]
 findValidMap [] _ = Nothing
-findValidMap (map:maps) visits
-    | isValidMap map visits = Just map
-    | otherwise = findValidMap maps visits
+findValidMap (zedMap:zedMaps) visits
+    | isValidMap zedMap visits = Just zedMap
+    | otherwise = findValidMap zedMaps visits
 
 
 -- generatePossibleMaps takes in a Int representing the lengnth of a side of the Zed map.
@@ -33,23 +33,48 @@ generatePossibleMaps n = concatMap permutations (choose (permutations [1..n]) []
         | otherwise = choose xs (x:result) ++ choose xs result
 
 
--- isValidMap takes a board in the form of a matrix and the merchant visits and returns
+-- isValidMap takes a map in the form of a matrix and the merchant visits and returns
 -- whether the map is valid or not according to rules of Zed.
 isValidMap :: (Num a, Ord a) => [[a]] -> [[a]] -> Bool
-isValidMap board merchantVisits =
-    and ( (zipWith (\x y -> numVisits y 0 == x) north n_dir) ++
-        (zipWith (\x y -> numVisits y 0 == x) west  w_dir) ++
-        (zipWith (\x y -> numVisits y 0 == x) south s_dir) ++
-        (zipWith (\x y -> numVisits y 0 == x) east  e_dir)  )
+isValidMap zedMap merchantVisits = (uniqueRowsAndCols zedMap) && (validMerchants zedMap merchantVisits)
+
+
+-- takes in a map and verifies that all rows and columns are unique
+uniqueRowsAndCols :: Ord a => [[a]] -> Bool
+uniqueRowsAndCols zedMap =
+    (foldr (\x y -> allUnique x && y) True rows ) &&
+    (foldr (\x y -> allUnique x && y) True columns)
   where
-    w_dir = board
-    n_dir = transpose board
+    rows = zedMap
+    columns = transpose zedMap
+
+
+-- checks whether all elements in a list are unique
+allUnique :: Ord t => [t] -> Bool
+allUnique [] = True
+allUnique (h:t)
+    | elem h t  = False
+    | otherwise = allUnique t
+
+
+-- takes in a map and a list of merchant visits and checks that
+-- the number of trading posts each merchant visits is consistent
+-- with the map
+validMerchants :: (Num a, Ord a) => [[a]] -> [[a]] -> Bool
+validMerchants zedMap visits =
+    and ( (zipWith (\x y -> numVisits y 0 == x) north n_dir) ++
+          (zipWith (\x y -> numVisits y 0 == x) west  w_dir) ++
+          (zipWith (\x y -> numVisits y 0 == x) south s_dir) ++
+          (zipWith (\x y -> numVisits y 0 == x) east  e_dir)  )
+  where
+    w_dir = zedMap
+    n_dir = transpose zedMap
     e_dir = map reverse w_dir
     s_dir = map reverse n_dir
-    north = merchantVisits !! 0
-    east = merchantVisits !! 1
-    south = reverse (merchantVisits !! 2)
-    west = reverse (merchantVisits !! 3)
+    north = visits !! 0
+    east = visits !! 1
+    south = reverse (visits !! 2)
+    west = reverse (visits !! 3)
 
 
 -- numVisits returns how many visits a merchant would make
